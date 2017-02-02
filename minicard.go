97853fcb785a5a60df843243ec205e4915747835
Pavel Borzenkov <pavel.borzenkov@gmail.com>
2017-02-02 18:09:15 +0300

    lingvo: add client method to get a word's minicard
    Signed-off-by: Pavel Borzenkov <pavel.borzenkov@gmail.com>

---
+++ minicard.go
@@ -0,0 +1,65 @@
+package lingvo
+
+import (
+	"context"
+)
+
+const (
+	endpointMinicard = "api/v1/Minicard"
+)
+
+// WordType is a type of word
+type WordType int
+
+// Types of words
+const (
+	None      WordType = 0
+	ExactWord WordType = 1 << (iota - 1)
+	LemmatizedVariant
+	Subphrase
+	SpellingVariant
+)
+
+// Minicard contains a minicard for a single word.
+type Minicard struct {
+	SourceLanguage Lang     `json:"SourceLanguage"`
+	TargetLanguage Lang     `json:"TargetLanguage"`
+	Heading        string   `json:"Heading"`
+	Translation    Word     `json:"Translation"`
+	SeeAlso        []string `json:"SeeAlso"`
+}
+
+// Word contains a translation of a single word.
+type Word struct {
+	Heading      string   `json:"Heading"`
+	Translation  string   `json:"Translation"`
+	Dictionary   string   `json:"DictionaryName"`
+	Sound        string   `json:"SoundName"`
+	Type         WordType `json:"Type"`
+	OriginalWord string   `json:"OriginalWord"`
+}
+
+// GetMinicard returns a minicard for word.
+func (c *Client) GetMinicard(ctx context.Context, word string, from, to Lang) (*Minicard, error) {
+	u, err := addOptions(endpointMinicard,
+		option{"text", word},
+		option{"srcLang", from.code()},
+		option{"dstLang", to.code()},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := c.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	minicard := new(Minicard)
+	err = c.Do(ctx, req, minicard)
+	if err != nil {
+		return nil, err
+	}
+
+	return minicard, nil
+}
---
+++ minicard_test.go
@@ -0,0 +1,66 @@
+package lingvo
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestGetMinicard(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/"+endpointMinicard, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		testFormValues(t, r, values{
+			"text":    "冬",
+			"srcLang": Ch.code(),
+			"dstLang": Ru.code(),
+		})
+
+		fmt.Fprintf(w, testMinicardJSON)
+	})
+
+	s, err := client.GetMinicard(context.Background(), "冬", Ch, Ru)
+	if err != nil {
+		t.Errorf("unexpected error '%v'", err)
+	}
+
+	if got, want := s, testMinicard; !reflect.DeepEqual(got, want) {
+		t.Errorf("unexpected result, want = %+v, got = %+v", want, got)
+	}
+}
+
+var testMinicard = &Minicard{
+	SourceLanguage: Ch,
+	TargetLanguage: Ru,
+	Heading:        "冬",
+	Translation: Word{
+		Heading:      "冬",
+		Translation:  "зима́; зи́мний",
+		Dictionary:   "Universal (Ch-Ru)",
+		Sound:        "",
+		Type:         ExactWord,
+		OriginalWord: "",
+	},
+	SeeAlso: []string{},
+}
+
+var testMinicardJSON = `
+{
+  "SourceLanguage": 1028,
+  "TargetLanguage": 1049,
+  "Heading": "冬",
+  "Translation": {
+    "Heading": "冬",
+    "Translation": "зима́; зи́мний",
+    "DictionaryName": "Universal (Ch-Ru)",
+    "SoundName": "",
+    "Type": 1,
+    "OriginalWord": ""
+  },
+  "SeeAlso": []
+}
+`
