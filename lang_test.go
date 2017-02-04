f6a27fffc98c8333505400596e6c30d370b95f8b
Pavel Borzenkov <pavel.borzenkov@gmail.com>
2017-02-04 18:54:40 +0300

    lingvo: fix lang test after recent rename
    Signed-off-by: Pavel Borzenkov <pavel.borzenkov@gmail.com>

--- lang_test.go
+++ lang_test.go
@@ -7,7 +7,7 @@ import (
 func TestLangByAbbr(t *testing.T) {
 	for _, l := range Languages {
 		abbr := l.String()
-		rl, err := LangByAbbr(abbr)
+		rl, err := LangByAbbrev(abbr)
 		if err != nil {
 			t.Errorf("unexpected error '%v' for lang '%s'", err, l)
 		}
@@ -16,7 +16,7 @@ func TestLangByAbbr(t *testing.T) {
 		}
 	}
 
-	_, err := LangByAbbr("??")
+	_, err := LangByAbbrev("??")
 	if err != ErrUnsupportedLanguage {
 		t.Errorf("unexpected language for '??', want = %v, got = %v", ErrUnsupportedLanguage, err)
 	}
